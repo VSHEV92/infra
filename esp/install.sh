681cfd70e6496319d4702c5117787a960355273c
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2025-07-24 10:29:27 +0300

    Update install.sh

--- esp/install.sh
+++ esp/install.sh
@@ -17,6 +17,11 @@ echo 'source ~/esp/esp-idf/export.sh' >> ~/.zshrc
 cd /etc/udev/rules.d
 sudo wget https://github.com/espressif/openocd-esp32/blob/master/contrib/60-openocd.rules
 
+cat << EOF | sudo tee /etc/udev/rules.d/70-esp32s3-jtag.rules
+ATTRS{idVendor}=="303a", ATTRS{idProduct}=="1001", MODE="0660", GROUP="plugdev", TAG+="uaccess"
+EOF
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
 # add user to dialout and plugdev groups
 sudo usermod -a -G dialout $USER
 sudo usermod -a -G plugdev $USER
